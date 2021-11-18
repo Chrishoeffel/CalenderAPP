cbd7a2898152ba3af6d692f9c915f81727804da9
Chris Ladd <codebyladd@gmail.com>
2021-11-18 12:55:25 -0700

    editing models

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,6 @@
 import os
 
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, User
 from django.contrib.auth.models import UnicodeUsernameValidator
 from django.core.validators import MinLengthValidator
 from django.db import models
@@ -9,14 +9,12 @@ from django.utils.translation import gettext_lazy as _
 
 class Avatar(models.Model):
     photo = models.ImageField(upload_to="avatars")
-    
 
     def __str__(self):
         return os.path.basename(self.photo.name)
 
 
 class User(AbstractUser):
-    user = models.OneToOneField(User, related_name="profile", on_delete=models.cascade)
     username = models.CharField(
         _("username"),
         max_length=150,
@@ -47,10 +45,12 @@ class Profile(models.Model):
         verbose_name = _('Profile')
         verbose_name_plural = _('Profiles')
 class Calendar_Event(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
+    name = models.ForeignKey(Profile, on_delete=models.CASCADE, serialize=True, max_length=100, blank=True)
+    event_date = models.DateField(auto_now_add=True)
+    time = models.DurationField(max_value=None, min_value=None)
+    update = models.DateTimeField(auto_now=True)
+    location = models.CharField(max_length=120)
+    decription = models.CharField(blank=True, max_length=300)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
