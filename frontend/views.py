7e1fd4f98295f95cc65aa827f78e9811e215dbc0
Chris Ladd <codebyladd@gmail.com>
2021-11-18 11:47:05 -0700

    Updating Requirements

--- frontend/views.py
+++ frontend/views.py
@@ -1,6 +1,9 @@
-import requests
+from datetime import datetime
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponse
+from django.views import generic
+from django.utils.safestring import make_safe
 # Create your views here.
 # def index(request):
 #     return render(request, 'frontend/index.html')
@@ -11,7 +14,29 @@ class IndexView(View):
     def get(self, request):
         return render(request, 'frontend/index.html') #this file is placed in templates of project dir 
 
-def index(request):
-    r = requests.get('http://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>') 
+from .models import *
+from .utils import Calendar
+
+class CalendarView(generic.ListView):
+    model = Event
+    template_name = 'cal/calendar.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # use today's date for the calendar
+        d = get_date(self.request.GET.get('day', None))
+
+        # Instantiate our calendar class with today's year and date
+        cal = Calendar(d.year, d.month)
+
+        # Call the formatmonth method, which returns our calendar as a table
+        html_cal = cal.formatmonth(withyear=True)
+        context['calendar'] = mark_safe(html_cal)
+        return context
+
+def get_date(req_day):
+    if req_day:
+        year, month = (int(x) for x in req_day.split('-'))
+        return date(year, month, day=1)
+    return datetime.today()
