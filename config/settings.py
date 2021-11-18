a1029cd846c23c6e05f44a463316a9e765185ca6
Chris Ladd <codebyladd@gmail.com>
2021-11-17 20:00:34 -0700

    Added setup for heroku new files

--- config/settings.py
+++ config/settings.py
@@ -100,6 +100,8 @@ if IS_POSTGRESQL is True:
         'default': {
             'ENGINE': env('DATABASE_ENGINE'),
             'NAME': env('DATABASE_NAME'),
+            'HOST': env('DATABASE_HOST'),
+            'PORT': env('DATABASE_PORT'),
         }
     }
 else:
