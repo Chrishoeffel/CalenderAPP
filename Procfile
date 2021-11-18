1734ac5cb123c1ef3db125a9733a0b6cb041e430
Chris Ladd <codebyladd@gmail.com>
2021-11-17 21:34:08 -0700

    removed log from Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn calanderAPP.wsgi --log-file -
+web: gunicorn calanderAPP.wsgi 
--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,4 @@
+import requests
 from django.shortcuts import render
 from django.views import View
 # Create your views here.
@@ -9,3 +10,8 @@ class IndexView(View):
     
     def get(self, request):
         return render(request, 'frontend/index.html') #this file is placed in templates of project dir 
+
+def index(request):
+    r = requests.get('http://httpbin.org/status/418')
+    print(r.text)
+    return HttpResponse('<pre>' + r.text + '</pre>') 
